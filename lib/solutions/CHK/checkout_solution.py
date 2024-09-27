e928a14a978609028fc2b293ecec347cc9fdaf37
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 18:00:06 +0000

    Fri Sep 27 18:00:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,10 +13,18 @@ def checkout(skus):
     df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special Offers': ['3A for 130', '2B for 45', '', '']})
     df_stock['Offer Number'] = df_stock['Special Offers'].apply(lambda x: x[:1])
     df_stock['Offer Price'] = df_stock['Special Offers'].apply(lambda x: get_offer_price(x))
+
+    total_price = 0
+    items = skus.split(',')
+    for item in skus:
+        df = df_stock[df_stock['Item'] == item]
+
+
     df_stock
 
 
 checkout('A, B')
+
 
 
 
