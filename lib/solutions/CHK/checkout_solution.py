e8bb159e01adc029d8c15eaae32751ef16e05ac1
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 18:51:06 +0000

    Fri Sep 27 18:51:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,31 +10,30 @@ def get_offer_info(row, key):
 # skus = unicode string
 def checkout(skus):
     df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special Offers': ['3A for 130', '2B for 45', pd.NA, pd.NA]})
-    # df_stock['Offer Number'] = df_stock['Special Offers'].apply(lambda x: x[:1])
-    # df_stock['Offer Price'] = df_stock['Special Offers'].apply(lambda x: get_offer_price(x))
-
-
-    # items = skus.split(', ')
-    # items = [item.strip() for item in items]
-    item_counts = {}
-    total_price = 0
-    for sku in df_stock['Item'].unique():
-        item_counts[sku] = skus.count(sku)
-    for key in item_counts:
-        df = df_stock[df_stock['Item'] == key]
-        count = item_counts.get(key)
-        if len(df[~df['Special Offers'].isna()]) > 0 :
-            offer_num, offer_price = get_offer_info(df['Special Offers'].iloc[0], key)
-        if offer_num:
-            if count % offer_num == 0:
-                total_price += offer_price * (count/offer_num)
+    if isinstance(skus, str):
+        item_counts = {}
+        total_price = 0
+        for sku in df_stock['Item'].unique():
+            item_counts[sku] = skus.count(sku)
+        for key in item_counts:
+            df = df_stock[df_stock['Item'] == key]
+            count = item_counts.get(key)
+            if len(df[~df['Special Offers'].isna()]) > 0 :
+                offer_num, offer_price = get_offer_info(df['Special Offers'].iloc[0], key)
+            if offer_num:
+                if count % offer_num == 0:
+                    total_price += offer_price * (count/offer_num)
+                else:
+                    while count > offer_num:
+                        total_price += offer_price
+                        count -= offer_num
+                    total_price += df['Price'].iloc[0] * count
             else:
-                while count > offer_num:
-                    total_price += offer_price
-                    count -= offer_num
                 total_price += df['Price'].iloc[0] * count
-        else:
-            total_price += df['Price'].iloc[0] * count
+
+        return int(total_price)
+    else:
+        return -1
 
 
     df_stock
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,21 @@
+from solutions.CHK import checkout_solution
+
+
+class TestChk():
+    def test_singles(self):
+        assert checkout_solution.checkout('A,B,C,D') == 35
+
+    def test_offers(self):
+        assert checkout_solution.checkout('A,A,A,B,B,C,D') == 210
+
+    def test_multiples(self):
+        assert checkout_solution.solution('A,A,A,A,A,A,B,B,B,B,B,C,D') == 415
+
+    def test_no_sku(self):
+        assert checkout_solution.solution('A,B') == 80
+
+    def test_illegal_input(self):
+        assert checkout_solution.solution(['A', 'B', 'C', 'D']) == -1
+
+    def test_string_format(self):
+        assert 
