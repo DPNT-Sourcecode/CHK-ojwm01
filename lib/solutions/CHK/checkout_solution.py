76d28ba81cc40d4ca93b71344ac8010559b9be9a
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 17:45:06 +0000

    Fri Sep 27 17:45:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,12 @@
-
+import pandas as pd
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    df_stock = pd.DataF
+    df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special Offers': ['3A for 130', '2B for 45']})
+    df_stock
+
+
+checkout('A, B')
+
 
