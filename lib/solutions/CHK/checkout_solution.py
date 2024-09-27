1e68d476f466caedb07b20217e18204331bd64d7
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 18:30:06 +0000

    Fri Sep 27 18:30:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,18 +1,14 @@
 import pandas as pd
 
 
-def get_offer_price(row):
-    price = row.split('for')
-    if price != ['']:
-        return int(price[1].strip())
-    else:
-        return ''
+def get_offer_info(row):
+    return row.split('for')[0].strip(), int(row.split('for')[1].strip())
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special Offers': ['3A for 130', '2B for 45', '', '']})
-    df_stock['Offer Number'] = df_stock['Special Offers'].apply(lambda x: x[:1])
-    df_stock['Offer Price'] = df_stock['Special Offers'].apply(lambda x: get_offer_price(x))
+    # df_stock['Offer Number'] = df_stock['Special Offers'].apply(lambda x: x[:1])
+    # df_stock['Offer Price'] = df_stock['Special Offers'].apply(lambda x: get_offer_price(x))
 
 
     # items = skus.split(', ')
@@ -24,7 +20,8 @@ def checkout(skus):
     for key in item_counts:
         df = df_stock[df_stock['Item'] == key]
         count = item_counts.get(key)
-        if dfd
+        if len(df[~df['Special Offers'].isna()]) > 0 :
+            offer_num, offer_price = get_offer_info(df['Special Offers'].iloc[0])
         if count == int(df['Offer Number'].iloc[0]):
             total_price += df['Offer Price'].iloc[0]
         else:
@@ -37,4 +34,5 @@ def checkout(skus):
 
 
 checkout('A, B, A, A')
+
 
