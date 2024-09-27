edef19f6f840abc6f0f03cacee8691745d2973c3
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 18:36:06 +0000

    Fri Sep 27 18:36:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,8 +25,11 @@ def checkout(skus):
         count = item_counts.get(key)
         if len(df[~df['Special Offers'].isna()]) > 0 :
             offer_num, offer_price = get_offer_info(df['Special Offers'].iloc[0], key)
-        if count == offer_num:
+        if count % offer_num == 0:
+            total_price += offer_price * (count/offer_num)
+        elif count > offer_num:
             total_price += offer_price
+            count -= offer_num
         else:
             total_price += df['Price'].iloc[0] * count
 
@@ -36,7 +39,8 @@ def checkout(skus):
     df_stock
 
 
-checkout('A, B, A, A')
+checkout('A, B, A, A, A, A, A,B,B')
+
 
 
 
