c78cd5fd4006aac2353e1eb93fa25f4f5326c15c
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 19:15:39 +0000

    Fri Sep 27 19:15:39 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -3,22 +3,22 @@ from solutions.CHK import checkout_solution
 
 class TestChk():
     def test_singles(self):
-        assert checkout_solution.checkout('A,B,C,D') == 115
+        assert checkout_solution.checkout('ABCD') == 115
 
     def test_offers(self):
-        assert checkout_solution.checkout('A,A,A,B,B,C,D') == 210
+        assert checkout_solution.checkout('AAABBCD') == 210
 
     def test_multiples(self):
-        assert checkout_solution.checkout('A,A,A,A,A,A,B,B,B,B,B,C,D') == 415
+        assert checkout_solution.checkout('AAAAAABBBBBCD') == 415
 
     def test_no_sku(self):
-        assert checkout_solution.checkout('A,B') == 80
+        assert checkout_solution.checkout('AB') == 80
 
     def test_illegal_input(self):
-        assert checkout_solution.checkout(['A', 'B', 'C', 'D']) == -1
+        assert checkout_solution.checkout('A,B,C,D') == -1
         assert checkout_solution.checkout(["ABCa"]) == -1
         assert checkout_solution.checkout(["-"]) == -1
 
-    def test_string_format(self):
-        assert checkout_solution.checkout('ABCD') == 115
-        assert checkout_solution.checkout('A;B;C;D') == 115
+    # def test_string_format(self):
+    #     assert checkout_solution.checkout('ABCD') == 115
+    #     assert checkout_solution.checkout('A;B;C;D') == 115
