ff9d772394426f0be849fd386885faf9db5c3ffc
sbx_user1051 <sbx_user1051@169.254.73.197>
2024-09-27 17:54:06 +0000

    Fri Sep 27 17:54:06 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,12 @@ import pandas as pd
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special_Offers': ['3A for 130', '2B for 45', '', '']})
-    df_stock['Offer Number'] = df_stock.Special_Offers.apply(lambda x: x[:1])
+    df_stock = pd.DataFrame({'Item': ['A', 'B', 'C', 'D'], 'Price': [50, 30, 20, 15], 'Special Offers': ['3A for 130', '2B for 45', '', '']})
+    df_stock['Offer Number'] = df_stock['Special Offers'].apply(lambda x: x[:1])
+    df_stock['Offer Price'] = df_stock['Special Offers'].apply(lambda x: x.split('for')[1] if x.split('for') != '')
     df_stock
 
 
 checkout('A, B')
+
 
