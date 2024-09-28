164eb936aef760b7b6aef9bca6f65a11f8561253
sbx_user1051 <sbx_user1051@169.254.80.117>
2024-09-28 15:55:54 +0000

    Sat Sep 28 15:55:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -80,7 +80,7 @@ def checkout(skus):
                             offer_get['item_free'] = 0
                     else:
                         count_free = count
-                        while count > offer_get['offer_num']:
+                        while count_free > offer_get['offer_num']:
                             item_counts[offer_get['item_free']] -= offer_get['amount_free']
                             count_free -= offer_get['offer_num']
                     total_price += df['Price'].unique()[0] * count
